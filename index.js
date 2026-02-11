e79ef8e7457024bd7b0fb7daa192c91125d0ba95
cocaiconcacvippro-hub <cocaiconcacvippro@gmail.com>
2026-02-12 00:02:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,14 @@ const startBot = () => {
 };
 
 startBot();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Web server started");
+});
